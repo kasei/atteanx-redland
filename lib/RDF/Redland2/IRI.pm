f075b117f0dfd9e91b64d9b4366591c018482486
Gregory Todd Williams <greg@evilfunhouse.com>
2014-08-15 22:32:33 -0700

    Add comment to node classes warning against instantiation from perl code.

--- lib/RDF/Redland2/IRI.pm
+++ lib/RDF/Redland2/IRI.pm
@@ -5,6 +5,11 @@ package RDF::Redland2::IRI 0.001 {
 	use RDF::Redland2;
 	use Moose;
 
+	# NOTE: Objects of this class are not meant to be constructed from perl.
+	#       They should only be constructed from within the XS code that is a
+	#       part of this package, allowing an underlying raptor structure to be
+	#       associated with the perl-level object.
+	
 	has 'ntriples_string'	=> (is => 'ro', isa => 'Str', lazy => 1, builder => '_ntriples_string');
 	with 'Attean::API::IRI';
 }
--- lib/RDF/Redland2/Literal.pm
+++ lib/RDF/Redland2/Literal.pm
@@ -4,7 +4,12 @@ use warnings;
 package RDF::Redland2::Literal 0.001 {
 	use RDF::Redland2;
 	use Moose;
-
+	
+	# NOTE: Objects of this class are not meant to be constructed from perl.
+	#       They should only be constructed from within the XS code that is a
+	#       part of this package, allowing an underlying raptor structure to be
+	#       associated with the perl-level object.
+	
 	has 'ntriples_string'	=> (is => 'ro', isa => 'Str', lazy => 1, builder => '_ntriples_string');
 	with 'Attean::API::Literal';
 }
